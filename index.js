5cd23064721136f149cb304f601918a22f14a422
zhangxiao <zhangxiao@kyee.com.cn>
2018-06-10 15:05:51 +0800

    change comment

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-  // 使用了superagent来发起请求
+  // use superagent send request
   var superagent = require('superagent');
-  // 查询本机ip，这里需要根据实际情况选择get还是post
   var sreq = superagent.get('http://news.baidu.com/passport');
   res.writeHead(200, {
     'Access-Control-Allow-Origin': 'http://localhost:8080',
